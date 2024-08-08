3435845f6bfbf5063eb5c24ce554766a4a563580
Tavish Puri <tavishpuri@Tavishs-MacBook-Air.local>
2024-08-09 01:07:25 +1200

    implemented getter methods in car.cpp file

--- src/app/Car.cpp
+++ src/app/Car.cpp
@@ -7,3 +7,11 @@ Car::Car(const std::string &number_plate, const std::string &model,
   rental_fee_ = rental_fee;
   is_available_ = true;
 }
+
+std::string Car::get_number_plate() const { return number_plate_; }
+
+std::string Car::get_model() const { return model_; }
+
+double Car::get_rental_fee() const { return rental_fee_; }
+
+bool Car::is_available() const { return is_available_; }
