5ac44d6b50d3ddec5d8ed62724136adedbe7b6cb
Tavish Puri <tavishpuri@Tavishs-MacBook-Air.local>
2024-08-09 00:50:40 +1200

    created basic car.h template with constructor function defined and car attributes defined

--- src/app/Car.h
+++ src/app/Car.h
@@ -1,14 +1,18 @@
 #ifndef CAR_H
 #define CAR_H
 
-#pragma once
+#include <string>
 
 class Car {
  public:
-  Car();
-  ~Car();
+  Car(const std::string& number_plate, const std::string& model,
+      double rental_fee);
 
  private:
+  std::string number_plate_;
+  std::string model_;
+  double rental_fee_;
+  bool is_available_;
 };
 
 #endif
