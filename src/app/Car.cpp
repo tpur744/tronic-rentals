ed178eac8b4bf93252e7bd0181621d53bcc79880
Tavish Puri <tavishpuri@Tavishs-MacBook-Air.local>
2024-08-09 01:00:53 +1200

    implemented constructor function in car.cpp file

--- src/app/Car.cpp
+++ src/app/Car.cpp
@@ -1,11 +1,9 @@
 #include "Car.h"
 
-Car::Car()
-{
-
-}
-
-Car::~Car()
-{
-
+Car::Car(const std::string &number_plate, const std::string &model,
+         double rental_fee) {
+  number_plate_ = number_plate;
+  model_ = model;
+  rental_fee_ = rental_fee;
+  is_available_ = true;
 }
