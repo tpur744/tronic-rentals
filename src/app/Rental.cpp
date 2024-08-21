35e0f2f93f91f7c7dcb28f377b055133474b0413
Tavish Puri <tavishpuri@wifi-172-23-24-154.uoa-wifi.auckland.ac.nz>
2024-08-21 14:35:22 +1200

    rental class created and made subclass of Car

---
+++ src/app/Rental.cpp
@@ -0,0 +1,3 @@
+#include "Rental.h"
+
+Rental::Rental() {}
---
+++ src/app/Rental.h
@@ -0,0 +1,15 @@
+#ifndef RENTAL_H
+#define RENTAL_H
+#include "Car.h"
+
+#pragma once
+
+class Rental : public Car
+{
+ public:
+  Rental();
+
+ private:
+};
+
+#endif
