49751b77be4c3e5bef605ec8766eb9fd6300a768
Tavish Puri <tavishpuri@tavishs-air.lan>
2024-08-29 00:00:19 +1200

    created date class

--- src/app/app.cpp
+++ src/app/app.cpp
@@ -240,8 +240,6 @@ void App::CreateRental(const std::vector<std::string> options) {
                  start_date, end_date, customer_id, rental_reference);
   rentals_.push_back(new_rental);
 
-  car->SetAvailability(false);
-
   // Calculate the number of days rented
   int days_rented = DaysBetweenDates(start_date, end_date);
 
